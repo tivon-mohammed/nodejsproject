b988eb2c96f04f2f994189ce05603d1f6475c476
ainguyen <40418101+QuynhAi@users.noreply.github.com>
2021-03-25 12:24:41 -0700

    added navigation-bar

---
+++ app.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import mongoose from 'mongoose'
+
+//constants declared
+const app=express()
+const port=6500
+
+//mongoose connection 
+mongoose.connect('mongodb://127.0.0.1:27017/edureka',{useUnifiedTopology:true,useNewUrlParser:true})
+const connection=mongoose.connection;
+connection.once('open',()=>{
+    console.log("MongoDB connected!!!!")
+})
+
+
+//app configurations
+
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', (request, response) => {
+    response.render('index')
+})
+app.get('/sports', (request, response) => {
+    response.render('sports.ejs')
+})
+app.get('/contactus', (request, response) => {
+    response.render('contactus')
+})
+app.get('/aboutus', (request, response) => {
+    response.render('aboutus')
+})
+
+//start express app
+app.listen(port,()=>{
+    console.log("app started !!", port)
+})
