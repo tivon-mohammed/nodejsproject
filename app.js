f0520114e0fa5dcf423c65a921f58fe10ea0a791
Allenmac2811 <80910935+Allenmac2811@users.noreply.github.com>
2021-03-26 22:41:49 -0400

    First commit

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 import express from 'express'
 import mongoose from 'mongoose'
+//Allen imports
+import http from 'http'
+import path from 'path';
+import socketIO from 'socket.io'
+import {LocalStorage} from 'node-localstorage'
+import iplocate from 'node-iplocate'
+import publicIP from 'public-ip'
+let localstorage= new LocalStorage('./Scratch')
 
 //constants declared
 const app=express()
@@ -14,10 +22,11 @@ connection.once('open',()=>{
 
 
 //app configurations
-
-
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+
 
 app.get('/', (request, response) => {
     response.render('index')
@@ -32,7 +41,63 @@ app.get('/aboutus', (request, response) => {
     response.render('aboutus')
 })
 
-//start express app
-app.listen(port,()=>{
-    console.log("app started !!", port)
+
+
+app.get('/chatbox', (request, response) => {
+    response.render('chatbox.ejs')
 })
+//Allen code
+//index.html file
+//server for chat
+let server = http.createServer(app).listen(port,()=>{
+    console.log("chat is up on: ", port)
+})
+let io = socketIO(server)
+io.sockets.on('connection',(socket)=>{
+    //console.log("connected")
+    socket.on('nick',(nick)=>{
+        console.log(nick)
+        socket.nickname = nick
+        })
+
+        socket.on('chat',(data)=>{
+            
+            
+            //console.log(data)
+            publicIP.v4()
+                .then(ip=>{
+                    iplocate(ip)
+                    .then((results)=>{
+                        let city = JSON.stringify(results.city,null,2)
+                        localstorage.setItem('userLocal',city)
+                    })
+                })
+    
+            let nickname = socket.nickname?socket.nickname:'';
+                var time = new Date().toLocaleTimeString();
+                if(nickname){
+                    let list = socket.client.conn.server.clients
+                    let users = Object.keys(list)
+                    socket.emit('userList',users)
+
+            let payload={
+                message:data.message,
+                nick:nickname,
+                location:localstorage.getItem('userLocal'),
+                time:time
+            }
+            if(data.message){
+            socket.emit('chat',payload)
+            socket.broadcast.emit('chat',payload)
+            }
+          }
+     })
+})
+
+
+
+
+// //start express app
+// app.listen(port,()=>{
+//     console.log("app started !!", port)
+// })
