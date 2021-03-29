b741c4e376274cb34f7ccd109657ba6c4cd86319
tivon-mohammed01 <tivon.mohammed01@utrgv.edu>
2021-03-29 15:46:56 -0500

    weather app, no bootstrap

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ import express from 'express'
 import mongoose from 'mongoose'
 
 //constants declared
-const app=express()
-const port=6500
+const app = express()
+const port = 6500
+
+const request = require('request');
+const weatherUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29";
+
 
 //mongoose connection 
-mongoose.connect('mongodb://127.0.0.1:27017/edureka',{useUnifiedTopology:true,useNewUrlParser:true})
-const connection=mongoose.connection;
-connection.once('open',()=>{
+mongoose.connect('mongodb://127.0.0.1:27017/edureka', { useUnifiedTopology: true, useNewUrlParser: true })
+const connection = mongoose.connection;
+connection.once('open', () => {
     console.log("MongoDB connected!!!!")
 })
 
@@ -32,7 +36,51 @@ app.get('/aboutus', (request, response) => {
     response.render('aboutus')
 })
 
+app.get('/weather', (req, res) => {
+    var dataPromise = getWeather();
+    dataPromise.then(JSON.parse)
+        .then(function(result) {
+            res.render('weather', { result, title: '***Weather App***' })
+        })
+})
+
 //start express app
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log("app started !!", port)
 })
+
+
+function getWeather(url) {
+    // Setting URL and headers for request
+    var options = {
+        url: weatherUrl,
+        headers: {
+            'User-Agent': 'request'
+        }
+    };
+    // Return new promise 
+    return new Promise(function(resolve, reject) {
+        // Do async job
+        request.get(options, function(err, resp, body) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(body);
+            }
+        })
+    })
+}
+
+
+
+app.get('/weatherwithoutpromise', (req, res) => {
+    request(url, (err, response, body) => {
+        if (err) {
+            console.log(err);
+        } else {
+
+            const output = JSON.parse(body);
+            res.send(output);
+        }
+    });
+});
